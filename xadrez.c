feb1736ab6d2297b452bd489f752e434b3388b2b
Matheus R. Ouverney <162365159+Matheuzin18@users.noreply.github.com>
2025-04-20 19:29:19 +0000

    xadrez nivel novato

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,38 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    int torre = 1; //Posição inicial da torre
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    while (torre <= 5) //Posição final da torre
+    {
+        printf("Movimento da Torre: Direita\n");
+        torre++;
+    }
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    printf("\n"); //Espaçamento entre o movimento das peças diferentes para deixar mais organizado
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+    int bispo = 1; //Posição inicial do bispo
+
+    do {
+        printf("Movimento do Bispo: Cima Direita\n");
+        bispo++;
+
+    } while (bispo <= 5); //Posição final do bispo
+
+
+    printf("\n"); //Espaçamento entre o movimento das peças diferentes para deixar mais organizado
+
+
+    int rainha;
+
+    for (rainha = 1; rainha <= 8; rainha++) //Posição inicial e final da rainha, respectivamente
+    {
+        printf("Movimento da Rainha: Esquerda\n");
+    }
 
     return 0;
 }
