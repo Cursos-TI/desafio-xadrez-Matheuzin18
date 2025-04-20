98c682873f782f55717ca42bbc89167e0ea13927
Matheus R. Ouverney <162365159+Matheuzin18@users.noreply.github.com>
2025-04-20 20:36:47 +0000

    xadrez nivel intermediário

--- xadrez.c
+++ xadrez.c
@@ -3,7 +3,7 @@
 
 int main() {
 
-    int torre = 1; //Posição inicial da torre
+    int torre = 1; //Variável e posição inicial da torre
 
     while (torre <= 5) //Posição final da torre
     {
@@ -15,7 +15,7 @@ int main() {
     printf("\n"); //Espaçamento entre o movimento das peças diferentes para deixar mais organizado
 
 
-    int bispo = 1; //Posição inicial do bispo
+    int bispo = 1; //Variável e posição inicial do bispo
 
     do {
         printf("Movimento do Bispo: Cima Direita\n");
@@ -27,12 +27,31 @@ int main() {
     printf("\n"); //Espaçamento entre o movimento das peças diferentes para deixar mais organizado
 
 
-    int rainha;
+    int rainha; //Variável da rainha
 
     for (rainha = 1; rainha <= 8; rainha++) //Posição inicial e final da rainha, respectivamente
     {
         printf("Movimento da Rainha: Esquerda\n");
     }
+
+
+    printf("\n"); //Espaçamento entre o movimento das peças diferentes para deixar mais organizado
+
+
+    int cavaloVertical, cavaloHorizontal; //Variáveis do cavalo
+
+    for(cavaloHorizontal = 1; cavaloHorizontal < 2; cavaloHorizontal++) //Movimento horizontal do cavalo
+    {
+
+        while (cavaloVertical < 2) //Movimento vertical do cavalo
+        {
+            printf("Movimento do vertical cavalo: Baixo\n");
+            cavaloVertical++;
+        }
+
+        printf("Movimento horizontal do cavalo: Esquerda\n");
+    }
+
 
     return 0;
 }
